69f89bb771cfe8d325cd8aab1a5d74a32cf457e8
ABCD <ABCtoDev@gmail.com>
2024-09-29 01:56:41 +0900

    tab plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,3 +19,42 @@ mapKey(">", ">gv", "v")
 -- split window
 mapKey("ss", ":split<Return>", "n")
 mapKey("vs", ":vsplit<Return>", "n")
+
+-- tab(bar) control
+mapKey("gT", "<Cmd>BufferPrevious<CR>")
+mapKey("gt", "<Cmd>BufferNext<CR>")
+mapKey("t<", "<Cmd>BufferMovePrevious<CR>")
+mapKey("t>", "<Cmd>BufferMoveNext<CR>")
+mapKey("t1", "<Cmd>BufferGoto 1<CR>")
+mapKey("t2", "<Cmd>BufferGoto 2<CR>")
+mapKey("t3", "<Cmd>BufferGoto 3<CR>")
+mapKey("t4", "<Cmd>BufferGoto 4<CR>")
+mapKey("t5", "<Cmd>BufferGoto 5<CR>")
+mapKey("t6", "<Cmd>BufferGoto 6<CR>")
+mapKey("t7", "<Cmd>BufferGoto 7<CR>")
+mapKey("t8", "<Cmd>BufferGoto 8<CR>")
+mapKey("t9", "<Cmd>BufferGoto 9<CR>")
+mapKey("t0", "<Cmd>BufferLast<CR>")
+-- Pin/unpin buffer
+-- mapKey("<A-p>", "<Cmd>BufferPin<CR>")
+-- Goto pinned/unpinned buffer
+--                 :BufferGotoPinned
+--                 :BufferGotoUnpinned
+-- Close buffer
+mapKey("tc", "<Cmd>BufferClose<CR>")
+-- Wipeout buffer
+--                 :BufferWipeout
+-- Close commands
+--                 :BufferClosetllButCurrent
+--                 :BufferClosetllButPinned
+--                 :BufferClosetllButCurrentOrPinned
+--                 :BufferCloseBuffersLeft
+--                 :BufferCloseBuffersRight
+-- Magic buffer-picking mode
+-- mapKey("<C-p>", "<Cmd>BufferPick<CR>")
+-- Sort automatically by...
+mapKey("<Space>tb", "<Cmd>BufferOrderByBufferNumber<CR>")
+mapKey("<Space>tn", "<Cmd>BufferOrderByName<CR>")
+mapKey("<Space>td", "<Cmd>BufferOrderBytirectory<CR>")
+mapKey("<Space>tl", "<Cmd>BufferOrderByLanguage<CR>")
+mapKey("<Space>tw", "<Cmd>BufferOrderByWindowNumber<CR>")
