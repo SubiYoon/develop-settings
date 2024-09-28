1f2831c259d6f5c1a508267b815fa774f4b9bf83
ABCD <ABCtoDev@gmail.com>
2024-09-29 01:28:22 +0900

    keyMapping add

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,17 +1,21 @@
 local mapKey = require("utils/keyMapper").mapKey
 
 -- Neotree toggle
-mapKey('<leader>e', ':Neotree toggle<cr>')
+mapKey("<leader>e", ":Neotree toggle<cr>")
 
 -- pane navigation
-mapKey('<C-h>', '<C-w>h') -- left
-mapKey('<C-j>', '<C-w>j') -- left
-mapKey('<C-k>', '<C-w>k') -- left
-mapKey('<C-l>', '<C-w>l') -- left
+mapKey("<C-h>", "<C-w>h") -- left
+mapKey("<C-j>", "<C-w>j") -- left
+mapKey("<C-k>", "<C-w>k") -- left
+mapKey("<C-l>", "<C-w>l") -- left
 
 -- clear search highlight
-mapKey('<leader>h', ':nohlsearch<CR>')
+mapKey("<leader>h", ":nohlsearch<CR>")
 
 -- indent
-mapKey('<', '<gv', 'v')
-mapKey('>', '>gv', 'v')
+mapKey("<", "<gv", "v")
+mapKey(">", ">gv", "v")
+
+-- split window
+mapKey("ss", ":split<Return>", "n")
+mapKey("vs", ":vsplit<Return>", "n")
