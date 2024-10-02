fe70a5fe96b316bb45e238c7915d329e86d3b8b4
ABCD <ABCtoDev@gmail.com>
2024-10-03 01:44:39 +0900

    notify

---
+++ lua/plugins/notify.lua
@@ -0,0 +1,27 @@
+return {
+  'rcarriga/nvim-notify',
+  config = function()
+    local notify = require('notify')
+    notify.setup({
+      background_colour = "NotifyBackground",
+      fps = 30,
+      icons = {
+        DEBUG = "",
+        ERROR = "",
+        INFO = "",
+        TRACE = "✎",
+        WARN = ""
+      },
+      level = 2,
+      minimum_width = 50,
+      render = "default",
+      stages = "fade_in_slide_out",
+      time_formats = {
+        notification = "%T",
+        notification_history = "%FT%T"
+      },
+      timeout = 5000,
+      top_down = true
+    })
+  end
+}
