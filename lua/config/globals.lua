56057bf87ce4a4e21ed0a440dd91ffb829a2da9b
ABCD <ABCtoDev@gmail.com>
2024-10-15 18:21:36 +0900

    auto complete option edit

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -4,3 +4,4 @@ vim.opt.shell = "/bin/zsh"
 vim.opt.shellcmdflag = "-ic"
 vim.opt.shellxquote = ""
 vim.g.transparency = 0.8
+vim.opt.completeopt = { "menuone", "noinsert" } -- auto complete view but not select
