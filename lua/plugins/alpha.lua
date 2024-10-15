4cc5502d432df583f8dedd9092c74e4d4f185d67
ABCD <ABCtoDev@gmail.com>
2024-10-15 14:53:16 +0900

    add keymapping

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,43 +1,43 @@
 return {
-	{
-		"goolord/alpha-nvim",
-		-- dependencies = { 'echasnovski/mini.icons' },
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			local alpha = require("alpha")
-			local dashboard = require("alpha.themes.dashboard")
+  {
+    "goolord/alpha-nvim",
+    -- dependencies = { 'echasnovski/mini.icons' },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      local alpha = require("alpha")
+      local dashboard = require("alpha.themes.dashboard")
 
-			-- set header
-			dashboard.section.header.val = {
-				"                                                                           ",
-				"                                                                           ",
-				" █████╗ ██████╗  ██████╗    ████████╗ ██████╗     ██████╗ ███████╗██╗   ██╗",
-				"██╔══██╗██╔══██╗██╔════╝    ╚══██╔══╝██╔═══██╗    ██╔══██╗██╔════╝██║   ██║",
-				"███████║██████╔╝██║            ██║   ██║   ██║    ██║  ██║█████╗  ██║   ██║",
-				"██╔══██║██╔══██╗██║            ██║   ██║   ██║    ██║  ██║██╔══╝  ╚██╗ ██╔╝",
-				"██║  ██║██████╔╝╚██████╗       ██║   ╚██████╔╝    ██████╔╝███████╗ ╚████╔╝ ",
-				"╚═╝  ╚═╝╚═════╝  ╚═════╝       ╚═╝    ╚═════╝     ╚═════╝ ╚══════╝  ╚═══╝  ",
-				"                                                                           ",
-				"                                                                           ",
-				"                                                                           ",
-				"gcc : single line comment                     :Git                         ",
-				"gc  : multi line comment                      :Gdff != :Git diff           ",
-				"                                              :Gitt blame                  ",
-				"za  : single group fold/unfold                :Git <command>               ",
-				"zM  : all group fold                                                       ",
-				"zR  : all group unfold                                                     ",
-			}
+      -- set header
+      dashboard.section.header.val = {
+        "                                                                           ",
+        "                                                                           ",
+        " █████╗ ██████╗  ██████╗    ████████╗ ██████╗     ██████╗ ███████╗██╗   ██╗",
+        "██╔══██╗██╔══██╗██╔════╝    ╚══██╔══╝██╔═══██╗    ██╔══██╗██╔════╝██║   ██║",
+        "███████║██████╔╝██║            ██║   ██║   ██║    ██║  ██║█████╗  ██║   ██║",
+        "██╔══██║██╔══██╗██║            ██║   ██║   ██║    ██║  ██║██╔══╝  ╚██╗ ██╔╝",
+        "██║  ██║██████╔╝╚██████╗       ██║   ╚██████╔╝    ██████╔╝███████╗ ╚████╔╝ ",
+        "╚═╝  ╚═╝╚═════╝  ╚═════╝       ╚═╝    ╚═════╝     ╚═════╝ ╚══════╝  ╚═══╝  ",
+        "                                                                           ",
+        "                                                                           ",
+        "                                                                           ",
+        "gcc : single line comment                      Git(<leader>g)              ",
+        "gc  : multi line comment                       Tab(<leader>t)              ",
+        "                                               Debug(<leader>d)            ",
+        "za  : single group fold/unfold                 Treesitter(<leader>f)       ",
+        "zM  : all group fold                                                       ",
+        "zR  : all group unfold                                                     ",
+      }
 
-			-- set memu button
-			dashboard.section.buttons.val = {
-				dashboard.button("n", "📝  > New file", ":ene <BAR> startinsert <CR>"),
-				dashboard.button("f", "📁  > Find file", ":Telescope find_files<CR>"),
-				dashboard.button("w", "🔍  > Find Word", ":Telescope live_grep <CR>"),
-				dashboard.button("r", "🗂️  > Recent", ":Telescope oldfiles<CR>"),
-				dashboard.button("q", "🚪  > Quit NVIM", ":qa<CR>"),
-			}
+      -- set memu button
+      dashboard.section.buttons.val = {
+        dashboard.button("n", "📝  > New file", ":ene <BAR> startinsert <CR>"),
+        dashboard.button("f", "📁  > Find file", ":Telescope find_files<CR>"),
+        dashboard.button("w", "🔍  > Find Word", ":Telescope live_grep <CR>"),
+        dashboard.button("r", "🗂️  > Recent", ":Telescope oldfiles<CR>"),
+        dashboard.button("q", "🚪  > Quit NVIM", ":qa<CR>"),
+      }
 
-			alpha.setup(dashboard.opts)
-		end,
-	},
+      alpha.setup(dashboard.opts)
+    end,
+  },
 }
