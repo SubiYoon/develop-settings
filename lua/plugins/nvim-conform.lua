cbe47c5186df07518a20ca2f022ae12adae8caf7
ABCD <ABCtoDev@gmail.com>
2024-10-16 17:17:54 +0900

    lsp & dap-ui edit

--- lua/plugins/nvim-conform.lua
+++ lua/plugins/nvim-conform.lua
@@ -1,21 +1,22 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
-		conform.setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				javascript = { { "prettierd", "prettier" } },
-				java = { { "prettierd", "prettier" } },
-				vue = { { "prettierd", "prettier" } },
-				html = { { "prettierd", "prettier" } },
-				xml = { { "prettierd", "prettier" } },
-			},
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_format = true,
-			},
-		})
-	end,
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
+        conform.setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+                javascript = { "prettierd", "prettier" },
+                java = { "google-java-format" },
+                css = { "prettierd", "prettier" },
+                vue = { "prettierd", "prettier" },
+                html = { "prettierd", "prettier" },
+                xml = { "prettierd", "prettier" },
+            },
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_format = "first",
+            },
+        })
+    end,
 }
