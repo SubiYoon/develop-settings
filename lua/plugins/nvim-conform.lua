88c5799580cd648a5f9475ff9e4226ec569e9bf9
ABCD <ABCtoDev@gmail.com>
2024-10-17 09:59:57 +0900

    conform edit

--- lua/plugins/nvim-conform.lua
+++ lua/plugins/nvim-conform.lua
@@ -6,11 +6,11 @@ return {
         conform.setup({
             formatters_by_ft = {
                 lua = { "stylua" },
-                javascript = { "prettierd", "prettier" },
+                javascript = {},
                 java = {},
                 css = { "prettierd", "prettier" },
                 vue = { "prettierd", "prettier" },
-                html = { "prettierd", "prettier" },
+                html = {},
                 xml = { "prettierd", "prettier" },
             },
             format_on_save = {
