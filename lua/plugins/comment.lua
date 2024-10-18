ba3e23c9f7aeb2e19eda9049f3b7d49a649b8e59
ABCD <ABCtoDev@gmail.com>
2024-10-18 18:12:53 +0900

    git cache delete
    mistake... all...

--- lua/plugins/comment.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "numToStr/Comment.nvim",
-  event = { "BufReadPre", "BufNewfile" },
-  opts = {
-    -- add any options here
-  },
-  lazy = false,
-  config = true,
-}
--- lua/plugins/language/java.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'nvim-java/nvim-java',
-    dependencies = { "neovim/nvim-lspconfig" },
-    priority = 1000, -- Ensure it loads first
-    config = function()
-        require("java").setup({})
-    end
-}
