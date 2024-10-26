e23f6d6429e4af051563d56787f314bde9fa893a
ABCD <ABCtoDev@gmail.com>
2024-10-26 23:31:22 +0900

    neovim auto newline edit

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -6,6 +6,7 @@ vim.opt.shell = "/bin/zsh"
 vim.opt.shellcmdflag = "-ic"
 vim.opt.shellxquote = ""
 vim.g.transparency = 0.8
+vim.opt.textwidth = 0
 -- 주석줄에서 Enter시 자동 주석 해제
 vim.opt_local.formatoptions:remove({ 'r', 'o' })
 -- 파일을 열었을 때 자동으로 Normal 모드로 전환
