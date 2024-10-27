97cac8b337ffb952d9e8b419466562b51a05b9a5
ABCD <ABCtoDev@gmail.com>
2024-10-27 14:30:27 +0900

    formatter 우선순위 수정

--- lua/plugins/nvim-conform.lua
+++ lua/plugins/nvim-conform.lua
@@ -14,7 +14,7 @@ return {
             },
             format_on_save = {
                 timeout_ms = 500,
-                lsp_format = "first",
+                lsp_format = "fallback",
             },
         })
     end,
