eb66a82b2af1d52623a79117ce3619a477c02577
ABCD <ABCtoDev@gmail.com>
2024-11-05 15:03:10 +0900

    dap breakpoint icon edit

--- lua/plugins/nvim-dap-ui.lua
+++ lua/plugins/nvim-dap-ui.lua
@@ -91,6 +91,11 @@ return {
     config = function(_, opts)
         local dap = require('dap')
 
+        -- 브레이크포인트 아이콘 설정
+        vim.fn.sign_define('DapBreakpoint', { text = '🔴', texthl = '', linehl = '', numhl = '' })
+        vim.fn.sign_define('DapBreakpointCondition', { text = '🟠', texthl = '', linehl = '', numhl = '' })
+        vim.fn.sign_define('DapBreakpointRejected', { text = '⚪', texthl = '', linehl = '', numhl = '' })
+
         require('dapui').setup(opts)
 
         dap.listeners.after.event_initialized["dapui_config"] = function()
@@ -132,15 +137,15 @@ return {
         }
 
         require("nvim-dap-virtual-text").setup {
-            enabled = true,             -- enable this plugin (the default)
-            enabled_commands = true,    -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+            enabled = true,                     -- enable this plugin (the default)
+            enabled_commands = true,            -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
             highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
-            highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
-            show_stop_reason = true,    -- show stop reason when stopped for exceptions
-            commented = false,          -- prefix virtual text with comment string
-            only_first_definition = true, -- only show virtual text at first definition (if there are multiple)
-            all_references = false,     -- show virtual text on all all references of the variable (not only definitions)
-            clear_on_continue = false,  -- clear virtual text on "continue" (might cause flickering when stepping)
+            highlight_new_as_changed = false,   -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+            show_stop_reason = true,            -- show stop reason when stopped for exceptions
+            commented = false,                  -- prefix virtual text with comment string
+            only_first_definition = true,       -- only show virtual text at first definition (if there are multiple)
+            all_references = false,             -- show virtual text on all all references of the variable (not only definitions)
+            clear_on_continue = false,          -- clear virtual text on "continue" (might cause flickering when stepping)
             --- A callback that determines how a variable is displayed or whether it should be omitted
             --- @param variable Variable https://microsoft.github.io/debug-adapter-protocol/specification#Types_Variable
             --- @param buf number
@@ -160,8 +165,8 @@ return {
             virt_text_pos = vim.fn.has 'nvim-0.10' == 1 and 'inline' or 'eol',
 
             -- experimental features:
-            all_frames = false, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
-            virt_lines = false, -- show virtual lines instead of virtual text (will flicker!)
+            all_frames = false,     -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+            virt_lines = false,     -- show virtual lines instead of virtual text (will flicker!)
             virt_text_win_col = nil -- position the virtual text at a fixed window column (starting from the first text column) ,
             -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
         }
