f98c929b0241a82fa71ba1fcdce39cf1957bcd36
ABCD <ABCtoDev@gmail.com>
2024-11-09 19:42:05 +0900

    cmp edit(함수, 생성자 자동완성시 괄호까지 자동완성)

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -39,7 +39,31 @@ return {
                     ["<C-u>"] = cmp.mapping.scroll_docs(4),
                     ["<C-Space>"] = cmp.mapping.complete(),
                     ["<C-e>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    -- ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    ['<CR>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            local entry = cmp.get_selected_entry()
+                            if entry then
+                                -- 선택된 항목의 종류가 함수나 생성자인 경우 후처리
+                                local completion_item = entry:get_completion_item()
+                                local is_function_like = completion_item.kind == cmp.lsp.CompletionItemKind.Function
+                                    or completion_item.kind == cmp.lsp.CompletionItemKind.Method
+                                    or completion_item.kind == cmp.lsp.CompletionItemKind.Constructor
+
+                                cmp.confirm({ select = false })
+
+                                -- 함수 또는 생성자일 경우 괄호 추가
+                                if is_function_like then
+                                    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("()<Left>", true, true, true),
+                                        'n', true)
+                                end
+                            else
+                                fallback()
+                            end
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
                     ["<Tab>"] = cmp.mapping.select_next_item(),
                     ["<S-Tab>"] = cmp.mapping.select_prev_item(),
                 }),
