e2982ba180b5ba244b19ebfd0d63fee9fc96e42d
ABCD <ABCtoDev@gmail.com>
2024-12-20 14:53:39 +0900

    html파일은 format하지 않음

--- lua/plugins/nvim-conform.lua
+++ lua/plugins/nvim-conform.lua
@@ -20,6 +20,15 @@ return {
 			format_on_save = {
 				timeout_ms = 500,
 				lsp_format = "fallback",
+				filter = function(bufnr)
+					-- bufnr이 유효한 숫자인지 확인
+					if type(bufnr) ~= "number" or not vim.api.nvim_buf_is_valid(bufnr) then
+						return false
+					end
+					-- 파일 형식 확인
+					local filetype = vim.bo[bufnr].filetype
+					return filetype ~= "html" -- HTML 제외
+				end,
 			},
 		})
 	end,
