851ae4f47bfca9250b5d46a17b97528dcd9032de
ABCD <abctodev@gmail.com>
2026-01-28 16:24:04 +0900

    dashboard edit

--- nvim/lua/plugins/neo-tree/init.lua
+++ nvim/lua/plugins/neo-tree/init.lua
@@ -62,6 +62,13 @@ return {
       bind_to_cwd = false,
       follow_current_file = { enabled = true },
       use_libuv_file_watcher = true,
+      filtered_items = {
+        visible = true,
+        never_show = {
+          ".DS_Store",
+          "Thumbs.db",
+        },
+      },
     },
     window = {
       position = "left",
